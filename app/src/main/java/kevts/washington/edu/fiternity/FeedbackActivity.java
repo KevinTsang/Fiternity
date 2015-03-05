3923b3aa23f592f2cd717ab368ecb8ed1dd63727
tchin94 <tchin1994@gmail.com>
2015-03-04 23:35:33 -0800

    Added function to smiley face
    feedback share fragment slides into frame when feedback is positive.
    It's not done yet

--- app/src/main/java/kevts/washington/edu/fiternity/FeedbackActivity.java
+++ app/src/main/java/kevts/washington/edu/fiternity/FeedbackActivity.java
@@ -1,5 +1,7 @@
 package kevts.washington.edu.fiternity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ImageButton;
 
 
 public class FeedbackActivity extends ActionBarActivity {
@@ -20,7 +23,7 @@ public class FeedbackActivity extends ActionBarActivity {
         setContentView(R.layout.activity_feedback);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.feedback_fragment, new PlaceholderFragment())
+                    .add(R.id.feedback_fragment, new FeedbackFragment())
                     .commit();
         }
     }
@@ -51,15 +54,38 @@ public class FeedbackActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class FeedbackFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public static FeedbackShareFragment newInstance() {
+            FeedbackShareFragment fragment = new FeedbackShareFragment();
+            Bundle args = new Bundle();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public FeedbackFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
             View rootView = inflater.inflate(R.layout.fragment_feedback, container, false);
+            ImageButton positiveBtn = (ImageButton) rootView.findViewById(R.id.feedback_positive_btn);
+            ImageButton negativeBtn = (ImageButton) rootView.findViewById(R.id.feedback_negative_btn);
+
+            positiveBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FeedbackShareFragment feedbackShareFragment =
+                            FeedbackShareFragment.newInstance();
+                    FragmentManager fragmentManager = getActivity().getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.setCustomAnimations(R.animator.slide_in_right, R.animator.slide_out_left);
+                    fragmentTransaction.replace(R.id.feedback_fragment, feedbackShareFragment);
+                    fragmentTransaction.commit();
+                }
+            });
             return rootView;
         }
     }
