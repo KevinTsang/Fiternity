84d0a2384837996580db024aa63081feb4201e2e
KevinTsang <kevin@tsangfamily.us>
2015-03-11 03:52:28 -0700

    Created and made progress on ProfileActivity.

--- app/src/main/java/kevts/washington/edu/fiternity/ProfileActivity.java
+++ app/src/main/java/kevts/washington/edu/fiternity/ProfileActivity.java
@@ -1,9 +1,15 @@
 package kevts.washington.edu.fiternity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
 
 
 public class ProfileActivity extends ActionBarActivity {
@@ -12,8 +18,29 @@ public class ProfileActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        populatePredefinedFields();
+        Button button = (Button)findViewById(R.id.toExerciseButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Save profile here
+                Intent intent = new Intent(ProfileActivity.this, ExerciseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    private void populatePredefinedFields() {
+        NumberPicker agePicker = (NumberPicker)findViewById(R.id.user_age);
+        agePicker.setMinValue(18);
+        agePicker.setMaxValue(99);
+
+        Spinner genderSpinner = (Spinner)findViewById(R.id.user_gender_selector);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, new String[] {"M", "F"});
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(adapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
