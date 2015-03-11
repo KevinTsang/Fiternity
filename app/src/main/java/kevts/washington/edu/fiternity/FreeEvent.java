2c0e2a1247661e6dc23cdb5d10190612d7b92b42
cvetand <cvetand@u.washington.edu>
2015-03-11 10:04:57 -0700

    requests are saved in the singleton

--- app/src/main/java/kevts/washington/edu/fiternity/FreeEvent.java
+++ app/src/main/java/kevts/washington/edu/fiternity/FreeEvent.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 /**
  * Created by cvetand on 3/10/15.
  */
@@ -46,4 +47,27 @@ public class FreeEvent implements Serializable {
 
     public void addUser(User user){ this.users.add(user);}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FreeEvent freeEvent = (FreeEvent) o;
+
+        if (endTime != null ? !endTime.equals(freeEvent.endTime) : freeEvent.endTime != null)
+            return false;
+        if (startTime != null ? !startTime.equals(freeEvent.startTime) : freeEvent.startTime != null)
+            return false;
+        if (users != null ? !users.equals(freeEvent.users) : freeEvent.users != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = users != null ? users.hashCode() : 0;
+        result = 31 * result + (startTime != null ? startTime.hashCode() : 0);
+        result = 31 * result + (endTime != null ? endTime.hashCode() : 0);
+        return result;
+    }
 }
--- app/src/main/java/kevts/washington/edu/fiternity/User.java
+++ app/src/main/java/kevts/washington/edu/fiternity/User.java
@@ -137,4 +137,29 @@ public class User implements Serializable {
         return user;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (age != user.age) return false;
+        if (gender != user.gender) return false;
+        if (zipCode != user.zipCode) return false;
+        if (!name.equals(user.name)) return false;
+        if (!phoneNumber.equals(user.phoneNumber)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + (int) gender;
+        result = 31 * result + phoneNumber.hashCode();
+        result = 31 * result + zipCode;
+        return result;
+    }
 }
