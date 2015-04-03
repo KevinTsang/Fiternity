065a8a3282a5de15fb9ce2c46761918ba5da1eac
KevinTsang <kevin@tsangfamily.us>
2015-04-03 13:12:04 -0700

    Added UI required for listview

--- app/src/main/java/kevts/washington/edu/fiternity/FiternityApplication.java
+++ app/src/main/java/kevts/washington/edu/fiternity/FiternityApplication.java
@@ -5,11 +5,21 @@ import android.app.Application;
 import com.facebook.FacebookSdk;
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 
 /**
  * Created by kevin on 3/31/15.
  */
 public class FiternityApplication extends Application {
+
+    private ParseUser parseUser;
+    private static FiternityApplication instance;
+
+    public FiternityApplication() {
+        if (instance == null)
+            instance = this;
+        else throw new RuntimeException("There cannot be multiple duplicates of this app running at once.");
+    }
 
     @Override
     public void onCreate() {
@@ -17,5 +27,9 @@ public class FiternityApplication extends Application {
         FacebookSdk.sdkInitialize(getApplicationContext());
         Parse.initialize(this);
         ParseFacebookUtils.initialize(this);
+    }
+
+    public static FiternityApplication getInstance() {
+        return instance;
     }
 }
